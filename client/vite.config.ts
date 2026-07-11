95ed6101f48b53590f37011eba6bcba546c64738
shaonexplorer <shaonexplorer@gmail.com>
2026-07-11 16:08:04 +0600

    implement .env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_BASE_URL || 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
